642f0c1685f31b81e9ec343a77d0448559071fb1
Lucrecious <luca.duran@hotmail.com>
2021-07-06 23:35:13 -0400

    Extents component recognizes capsule shape as well

--- components/extents.gd
+++ components/extents.gd
@@ -2,10 +2,19 @@ class_name Component_Extents
 extends CollisionShape2D
 
 
-onready var value := shape.extents as Vector2
+onready var value := Vector2.ZERO setget __null_set
 
 func _ready() -> void:
-	assert(shape is RectangleShape2D, 'only handles this type right now')
+	assert(shape is RectangleShape2D or shape is CapsuleShape2D, 'only handles these types right now')
+	if shape is RectangleShape2D:
+		value = shape.extents
+		return
+	
+	if shape is CapsuleShape2D:
+		value = Vector2(shape.radius, (shape.radius * 2.0 + shape.height) / 2.0)
+
 
 func get_as_global_rect() -> Rect2:
 	return Rect2(global_position - value, value * 2.0)
+
+func __null_set(__): return
