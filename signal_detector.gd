c3d6ec608b048efbac8c7373414788f7dd3ff2ab
Lucrecious <luca.duran@hotmail.com>
2021-08-26 23:17:18 -0400

    update signal dertector

--- signal_detector.gd
+++ signal_detector.gd
@@ -11,8 +11,8 @@ func raised() -> bool: return _signal_raised
 func _init(object: Object, signal_name: String).():
 	_signal_name = signal_name
 	_object = object
-	_object.connect(_signal_name, self, '_on_signal_called')
+	ObjEct.connect_once(_object, _signal_name, self, '_on_signal_called')
 
 func _on_signal_called(_1=null,_2=null,_3=null,_4=null,_5=null,_6=null) -> void:
-	_object.disconnect(_signal_name, self, '_on_signal_called')
+	ObjEct.disconnect_once(_object, _signal_name, self, '_on_signal_called')
 	_signal_raised = true
