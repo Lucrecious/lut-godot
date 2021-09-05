12d95d8933d12dd48b28929adeaca859a9c65586
unknown <luca.duran@hotmail.com>
2021-09-05 14:05:03 -0400

    Fix audio control at low levels

--- ui/audio_control.gd
+++ ui/audio_control.gd
@@ -8,25 +8,26 @@ export(String) var _master_bus_name := ''
 export(String) var _music_bus_name := ''
 export(String) var _sound_bus_name := ''
 
-const top_volume := 20.0
-const bottom_volume := -60.0
+const top_master_volume := 20.0
+const top_all_volume := 0.0
+const bottom_all_volume := -30.0
 
 func _ready() -> void:
 	if not _master_bus_name.empty():
 		_master.connect('value_changed', self, '_on_master_volume_changed')
-		_master.value = inverse_lerp(bottom_volume, top_volume, _get_bus_volume(_master_bus_name))
+		_master.value = inverse_lerp(bottom_all_volume, top_master_volume, _get_bus_volume(_master_bus_name))
 	else:
 		_master.visible = false
 	
 	if not _music_bus_name.empty():
 		_music.connect('value_changed', self, '_on_music_volume_changed')
-		_music.value = inverse_lerp(bottom_volume, top_volume, _get_bus_volume(_music_bus_name))
+		_music.value = inverse_lerp(bottom_all_volume, top_all_volume, _get_bus_volume(_music_bus_name))
 	else:
 		_music.visible = false
 	
 	if not _sound_bus_name.empty():
 		_sound.connect('value_changed', self, '_on_sound_volume_changed')
-		_sound.value = inverse_lerp(bottom_volume, top_volume, _get_bus_volume(_sound_bus_name))
+		_sound.value = inverse_lerp(bottom_all_volume, top_all_volume, _get_bus_volume(_sound_bus_name))
 	else:
 		_sound.visible = false
 
@@ -44,5 +45,11 @@ func _get_bus_volume(bus_name: String) -> float:
 
 func _set_volume_to_bus(bus_name: String, percent: float) -> void:
 	var bus_index := AudioServer.get_bus_index(bus_name)
-	AudioServer.set_bus_volume_db(bus_index, lerp(bottom_volume, top_volume, percent))
+	var top := top_master_volume if bus_name == _master_bus_name else top_all_volume
+	
+	if is_equal_approx(percent, 0.0):
+		AudioServer.set_bus_mute(bus_index, true)
+	else:
+		AudioServer.set_bus_mute(bus_index, false)
+	AudioServer.set_bus_volume_db(bus_index, lerp(bottom_all_volume, top, percent))
 
