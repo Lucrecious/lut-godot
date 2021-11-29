48230301a40e1cbe7286c216fe95277a9fe24eca
Lucrecious <luca.duran@hotmail.com>
2021-11-28 21:42:39 -0500

    update manager

--- turn_based_nodes/manager.gd
+++ turn_based_nodes/manager.gd
@@ -7,9 +7,16 @@ export(int) var turn := -1 setget _turn_set
 
 var players := []
 
+var _current_player := -1
+
 func _ready() -> void:
 	yield(get_tree().create_timer(2.0), 'timeout')
 	start()
+
+func get_current_player() -> Node:
+	if _current_player == -1:
+		return null
+	return players[_current_player]
 
 func start() -> void:
 	if turn > -1:
@@ -27,13 +34,6 @@ func register(node: Node, front: bool) -> void:
 	else:
 		players.push_back(node)
 
-func execute(command: TurnBased_Command) -> void:
-	command.execute()
-	
-	if command.go_to_next:
-		_turn_set(turn + 1)
-		return
-
 func _turn_set(value: int) -> void:
 	if value == turn:
 		return
@@ -42,5 +42,6 @@ func _turn_set(value: int) -> void:
 	
 	# Delay the signal for the idle frame
 	yield(get_tree(), 'idle_frame')
-	emit_signal('turn_changed', players[turn % players.size()])
+	_current_player = turn % players.size()
+	emit_signal('turn_changed')
 
