5dff0418a60de25d110507f3635540fcc54c5b4b
Lucrecious <luca.duran@hotmail.com>
2022-10-13 16:42:53 -0400

    Better span limiter'

--- ui/span_limiter.gd
+++ ui/span_limiter.gd
@@ -1,8 +1,8 @@
 class_name SpanLimiter
 extends MarginContainer
 
-const MAX_SPAN := 100_000
-const MIN_SPAN := 720
+export(float) var max_span := 100_000.0
+export(float) var min_span := 720.0
 
 func _init() -> void:
 	connect('resized', self, '_on_SpanLimiter_resized')
@@ -11,13 +11,13 @@ func _on_SpanLimiter_resized():
 	_limit_span()
 
 func _limit_span():
-	if rect_size.x > MAX_SPAN:
-		var half_margin = (rect_size.x - MAX_SPAN) / 2.0
+	if rect_size.x > max_span:
+		var half_margin = (rect_size.x - max_span) / 2.0
 		
 		set("custom_constants/margin_left", half_margin)
 		set("custom_constants/margin_right", half_margin)
-	elif rect_size.x < MIN_SPAN:
-		var half_margin = (MIN_SPAN - rect_size.x) / 2.0
+	elif rect_size.x < min_span:
+		var half_margin = (min_span - rect_size.x) / 2.0
 		
 		set("custom_constants/margin_left", -half_margin)
 		set("custom_constants/margin_right", -half_margin)
